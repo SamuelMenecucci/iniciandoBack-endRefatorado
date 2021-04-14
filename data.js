b6e41722dae80759ec280267d288066892336704
Samuel Menecucci <samuelmenecucci@gmail.com>
2021-04-14 14:43:22 -0300

    arquivo para estudo de exportação de dados

---
+++ data.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário animado com JS puro e CSS Animation | Mayk Brito",
+    duration: "57 Min",
+    price: "Free",
+    featured: true,
+  },
+  {
+    id: "K-8z_4xvT3o",
+    title: "Recriando interface do Twitter | Guilherme Rodz",
+    duration: "158 Min",
+    price: "Free",
+  },
+  {
+    id: "EhnXaybirdA",
+    title: "Criando um projeto do zero para seu portfólio | Mayk Brito",
+    duration: "44 Min",
+    price: "Free",
+  },
+  {
+    id: "mxIhSTP6ddE",
+    title:
+      "Utilizando UX para projetar uma aplicação do zero | Diego e Thainan",
+    duration: "78 Min",
+    price: "Free",
+    featured: true,
+  },
+  {
+    id: "rCeGfFk-uCk",
+    title: "Setup para apps Node.js (TypeScript, ESLint, Jest, Babel) | Diego",
+    duration: "45 Min",
+    price: "Free",
+  },
+  {
+    id: "QfcozcbDhNM",
+    title: "Técnicas de estudo e aprendizado tradicional | Podcast FalaDev #11",
+    duration: "43 Min",
+    price: "Free",
+    featured: true,
+  },
+]
